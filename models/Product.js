5900454371c9f3a5161bf18b085b027ae90983d4
LeShush <mycelium.architecture@gmail.com>
2023-08-12 18:14:31 -0600

    Bugfix - Operativity enabled

--- models/Product.js
+++ models/Product.js
@@ -1,47 +1,45 @@
 // import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require('../config/connection.js');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model { }
+class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
   {
     id: {
-      type: DataTypes.INTEGER, // Integer datatype
-      allowNull: false, // Doesn't allow null values
-      primaryKey: true, // Set as primary key
-      autoIncrement: true // Uses auto increment
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true, 
+      autoIncrement: true,
     },
     product_name: {
-      type: DataTypes.STRING, // String datatype
+      type: DataTypes.STRING,
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL, // Decimal datatype
+      type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
-        isDecimal: true // Checks for decimal
+        isDecimal: true,
       },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       validate: {
-        isNumeric: true, // Checks for numeric
+        isNumeric: true,
       },
-      defaultValue: 10, // Default value of 10
     },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'category', // References the category model
-        key: 'id', // References the id key
-        unique: false, // Doesn't have to be unique
+        model: 'category', 
+        key: 'id', 
       },
-    }
+    },
   },
   {
     sequelize,
